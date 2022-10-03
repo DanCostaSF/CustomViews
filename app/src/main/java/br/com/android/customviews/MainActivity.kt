91daed696caf7bf97f714ac9ffb3b8f13ec48ff5
Henrique Altmayer <henrique_altm@hotmail.com>
2022-10-03 14:26:35 -0300

    Atualizando LabelShowMoneyComponent

--- app/src/main/java/br/com/android/customviews/MainActivity.kt
+++ app/src/main/java/br/com/android/customviews/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.android.customviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.android.customviews.component.LabelShowMoneyComponent
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<LabelShowMoneyComponent>(R.id.lsmc).money = 123f
     }
+
 }
--- app/src/main/java/br/com/android/customviews/component/LabelShowMoneyComponent.kt
+++ app/src/main/java/br/com/android/customviews/component/LabelShowMoneyComponent.kt
@@ -2,21 +2,27 @@ package br.com.android.customviews.component
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import br.com.android.customviews.R
 import br.com.android.customviews.databinding.LabelShowMoneyBinding
 
+@SuppressLint("SetTextI18n")
 class LabelShowMoneyComponent @JvmOverloads constructor(
     context: Context,
     private val attrs: AttributeSet? = null,
     private val defStyle: Int = 0,
 ) : ConstraintLayout(context, attrs, defStyle) {
 
-    private val binding =
-        LabelShowMoneyBinding.inflate(LayoutInflater.from(context), this, true)
+    private val binding = LabelShowMoneyBinding.inflate(LayoutInflater.from(context), this, true)
+
+    var money: Float? = null
+        set(value) {
+            binding.txtValor.text = "R$ $value"
+            field = value
+        }
 
     init {
         setupAttr()
@@ -27,16 +33,22 @@ class LabelShowMoneyComponent @JvmOverloads constructor(
         val style = context.obtainStyledAttributes(
             attrs, R.styleable.LabelShowMoneyComponent, defStyle, 0
         )
-        val paramValue = style.getFloat(R.styleable.LabelShowMoneyComponent_money, 0f)
-        binding.txtValor.text = "R$ $paramValue"
+        updateParamValue(style)
+        onContainerClick()
+    }
 
+    private fun updateParamValue(style: TypedArray) {
+        money = style.getFloat(R.styleable.LabelShowMoneyComponent_money, 0f)
+    }
+
+    private fun onContainerClick() {
         binding.root.setOnClickListener {
-            if (binding.txtValor.visibility == View.GONE) {
-                binding.txtValor.visibility = View.VISIBLE
-                binding.txtvToqueVizualizar.visibility = View.GONE
+            if (binding.txtValor.visibility == GONE) {
+                binding.txtValor.visibility = VISIBLE
+                binding.txtvToqueVizualizar.visibility = INVISIBLE
             } else {
-                binding.txtValor.visibility = View.GONE
-                binding.txtvToqueVizualizar.visibility = View.VISIBLE
+                binding.txtValor.visibility = GONE
+                binding.txtvToqueVizualizar.visibility = VISIBLE
             }
         }
     }
